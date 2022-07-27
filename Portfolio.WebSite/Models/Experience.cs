6d437b4ab6cad7a160584f97a38fbefa2dfd5434
Joona Laitila <joona.laitila123@hotmail.com>
2022-07-27 11:57:36 +0300

    Added existing project files to Git

---
+++ Portfolio.WebSite/Models/Experience.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Portfolio.WebSite.Models
+{
+    public class Experience
+    {
+        public string Id { get; set; }
+        [JsonPropertyName("img")]
+        public string Image { get; set; }
+        public string url { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize<Experience>(this);
+        
+    }
+}
---
+++ Portfolio.WebSite/Pages/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Portfolio.WebSite.Models;
+using Portfolio.WebSite.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio.WebSite.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public JsonFilePortfolioService PortfolioService;
+        public IEnumerable<Experience> Experiences { get; private set; }
+
+        public IndexModel(
+            ILogger<IndexModel> logger,
+            JsonFilePortfolioService portfolioService)
+        {
+            _logger = logger;
+            PortfolioService = portfolioService;
+        }
+
+        public void OnGet()
+        {
+            Experiences = PortfolioService.GetExperience();
+        }
+    }
+}
